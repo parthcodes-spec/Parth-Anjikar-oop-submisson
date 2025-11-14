e2ca2794c68b0511fb693a44302dff7213553249
parthcodes-spec <parth.anjikar21@gmail.com>
2025-11-14 20:37:28 +0530

    Add files via upload

---
+++ Experiment no 7/Experiment 7.4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Number {
+private:
+    int x;
+public:
+    void input() {
+        cout << "Enter a number: ";
+        cin >> x;
+    }
+
+    void operator-() {
+        x = -x;
+    }
+
+    void display() {
+        cout << "Value = " << x << endl;
+    }
+};
+
+int main() {
+    Number n;
+    n.input();
+    -n;    // Unary minus operator
+    n.display();
+    return 0;
+}
